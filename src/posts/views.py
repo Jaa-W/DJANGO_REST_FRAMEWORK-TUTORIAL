1c5f9483bdab90b57465cca94be586d0e0185b02
JaaW <asiaw6@gmail.com>
2021-03-04 15:56:44 +0100

    views and URLs

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from posts.models import Post
+from posts.serializers import PostSerializer
 
-# Create your views here.
+@csrf_exempt
+def post_list(request):
+    if request.method == 'GET':                                 # SHOW ALL POST
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many = True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':                              # CREATE NEW POST
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status = 400)
+
+@csrf_exempt
+def post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk = pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status = 404)
+
+    if request.method == 'GET':                                 # SHOW POST'S DETAILS
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == 'PUT':                               # EDIT POST
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return  JsonResponse(serializer.errors, status = 400)
+
+    elif request.method == 'DELETE':
+        post.delate()
+        return HttpResponse(status = 204)
