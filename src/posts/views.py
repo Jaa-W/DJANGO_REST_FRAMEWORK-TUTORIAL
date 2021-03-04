e252cc8f23692931c71481b526309167b020ecc2
JaaW <asiaw6@gmail.com>
2021-03-04 20:10:24 +0100

    Using generic class-based views

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,37 +1,11 @@
-from django.shortcuts import render
-from django.http import Http404
-from rest_framework import status, mixins, generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import  generics
 from posts.models import Post
 from posts.serializers import PostSerializer
 
-class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    
-    def get(self, request, *arg, **kwargs):                # SHOW ALL POST
-        return self.list(request, *arg, **kwargs)
-
-    def post(self, request, *arg, **kwargs):               # CREATE NEW POST                          
-        return self.create(request, *arg, **kwargs)
-
-
-
-
-class PostDetail(
-    mixins.RetrieveModelMixin, 
-    mixins.UpdateModelMixin, 
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *arg, **kwargs):                # SHOW POST DETAILS
-        return self.retrieve(request, *arg, **kwargs)                           
-        
-    def put(self, request, *arg, **kwargs):                # EDIT POST
-        return self.update(request, *arg, **kwargs)
-
-    def delate(self, request, *arg, **kwargs):                # DELETE POST
-        return self.destroy(request, *arg, **kwargs)
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
