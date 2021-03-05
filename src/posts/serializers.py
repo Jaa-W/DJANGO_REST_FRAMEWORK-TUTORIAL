111857535b60a3115f4372cf1518a6657d4b5767
JaaW <asiaw6@gmail.com>
2021-03-05 10:49:04 +0100

    Hyperlinks and pagination

--- src/posts/serializers.py
+++ src/posts/serializers.py
@@ -3,19 +3,21 @@ from rest_framework import fields, serializers
 from posts.models import Post
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    posts = serializers.PrimaryKeyRelatedField(many = True, queryset = Post.objects.all())          # many beacouse users could have many posts
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    posts = serializers.HyperlinkedRelatedField(
+        many = True, read_only = True, view_name='post-detail')          # many beacouse users could have many posts
     owner = serializers.ReadOnlyField(source = 'owner.username')
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'posts', 'owner']
+        fields = ['url','id', 'username', 'posts', 'owner']
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source = 'owner.username')
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'author']
+        fields = ['url','owner', 'id', 'title', 'content', 'author']
     # class Meta replaced that actions
     # id = serializers.IntegerField(read_only = True)
     # title = serializers.CharField(required = True, allow_blank = False, max_length = 100)
--- src/posts/views.py
+++ src/posts/views.py
@@ -3,6 +3,16 @@ from posts.models import Post
 from django.contrib.auth.models import User
 from posts.permissions import IsOwnerOrReadOnly
 from posts.serializers import PostSerializer, UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def api_root(request, format = None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'posts': reverse('post-list', request=request, format=format)
+    })
 
 class UserList(generics.ListAPIView):                           # SHOW ALL USERS
     queryset = User.objects.all()
